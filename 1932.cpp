83e9cdfbca0b93b847a17431772f99fa44ebe13f
unknown <junsang.yu3@gmail.com>
2021-04-25 00:43:49 +0900

    1932

--- 1932.cpp
+++ 1932.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int n;
 int **arr;
+vector<vector<int>> v;
 
 int main() {
   cin >> n;
@@ -15,5 +17,22 @@ int main() {
       cin >> arr[i][j];
     }
   }
+  v.push_back({ arr[0][0] });
+  for (int i = 1; i < n; i++) {
+    vector<int> tmp;
+    for (int j = 0; j <= i; j++) {
+      if (j == 0) {
+        tmp.push_back(v[i - 1][j] + arr[i][j]);
+      }
+      else if (j == i) {
+        tmp.push_back(v[i - 1][j - 1] + arr[i][j]);
+      }
+      else {
+        tmp.push_back(max(v[i - 1][j - 1] + arr[i][j], v[i - 1][j] + arr[i][j]));
+      }
+    }
+    v.push_back(tmp);
+  }
+  cout << *max_element(v[n - 1].begin(), v[n - 1].end());
   return 0;
 }
