eb37af3980a8b4d5537d68ec7eec28111ba5e2c7
unknown <junsang.yu3@gmail.com>
2021-05-03 00:40:45 +0900

    2579

--- 2579.cpp
+++ 2579.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
   int n;
-  int isConnected = 2;
   vector<int> v, dp;
   cin >> n;
   for (int i = 0; i < n; i++) {
@@ -15,15 +15,9 @@ int main() {
   }
   dp.push_back(v.at(0));
   if (n > 1) dp.push_back(v.at(0) + v.at(1));
-  for (int i = 2; i < n; i++) {
-    if (dp.at(i - 2) > dp.at(i - 1) || isConnected == 2) {
-      dp.push_back(dp.at(i - 2) + v.at(i));
-      isConnected = 1;
-    }
-    else {
-      dp.push_back(dp.at(i - 1) + v.at(i));
-      isConnected += 1;
-    }
+  if (n > 2) dp.push_back(max(v.at(0), v.at(1)) + v.at(2));
+  for (int i = 3; i < n; i++) {
+    dp.push_back(max(dp.at(i - 3) + v.at(i - 1), dp.at(i - 2)) + v.at(i));
   }
   cout << dp.at(n - 1);
   return 0;
