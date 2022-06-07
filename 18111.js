0f0c7c7a56ac63b8238b44978a567db62dcf6418
JunSangYu <35032401+stupidJoon@users.noreply.github.com>
2022-06-07 23:22:37 +0900

    4th

--- 18111.js
+++ 18111.js
@@ -1,26 +1,36 @@
 const fs = require('fs');
 
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-const [N, M, B] = input[0].split(' ').map(Number);
-const arr = input.slice(1).flatMap((value) => value.split(' ').map(Number));
-
-let lowestTime = [Number.MAX_SAFE_INTEGER, -1];
-
-[...new Set(arr)].forEach((height) => {
-    let time = 0;
-    let availableBlocks = B;
-    arr.map((value) => value - height).forEach((value) => {
-        if (value < 0) {
-            time += value * -1;
-            availableBlocks -= value * -1;
-        } else if (value > 0) {
-            time += value * 2;
-        }
-    });
-    if (availableBlocks < 0) return;
-    if (time < lowestTime[0] && height > lowestTime[1]) {
-        lowestTime = [time, height];
-    }
+const [N, M, B] = input.shift().split(' ').map(Number);
+const arr = input.flatMap((value) => value.split(' ').map(Number));
+const map = { };
+arr.forEach((value) => {
+    if (map[value]) map[value] += 1;
+    else map[value] = 1;
 });
 
-console.log(lowestTime.join(' '));
+let lowestTime = Number.MAX_SAFE_INTEGER
+let lowestTimeHeight;
+
+for (let height = 256; height >= 0; height -= 1) {
+    let time = 0;
+    let availableBlocks = B;
+    Object.entries(map).forEach(([value, times]) => {
+        const diff = value - height;
+        availableBlocks += diff * times;
+        if (diff < 0) {
+            time += -diff * times;
+        }
+        if (diff > 0) {
+            time += diff * 2 * times;
+        }
+    });
+    if (availableBlocks < 0) continue;
+    if (time > lowestTime) break;
+    if (time < lowestTime) {
+        lowestTime = time;
+        lowestTimeHeight = height;
+    }
+}
+
+console.log(`${lowestTime} ${lowestTimeHeight}`);
