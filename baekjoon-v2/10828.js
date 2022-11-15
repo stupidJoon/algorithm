251b5b06d964216f550fc44508d3fbf72a90b2d0
JunSangYu <35032401+stupidJoon@users.noreply.github.com>
2022-11-15 13:25:36 +0000

    11.15 1

---
+++ baekjoon-v2/10828.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const arr = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+
+const stack = [];
+
+const log = [];
+
+arr.forEach((value) => {
+  let [opcode, operand] = value.split(' ');
+
+  operand = Number(operand);
+
+  ({
+    push: () => {
+      stack.push(operand);
+    },
+    pop: () => {
+      log.push(stack.pop() ?? -1);
+    },
+    size: () => {
+      log.push(stack.length);
+    },
+    empty: () => {
+      log.push(Number(stack.length === 0));
+    },
+    top: () => {
+      log.push(stack[stack.length - 1] ?? -1);
+    },
+  })[opcode]();
+});
+
+console.log(log.join('\n'));
