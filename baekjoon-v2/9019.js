822839e255023721efe00c3d9efb655b6d9d9b13
JunSangYu <35032401+stupidJoon@users.noreply.github.com>
2022-12-19 13:50:19 +0000

    12.19

--- baekjoon-v2/9019.js
+++ baekjoon-v2/9019.js
@@ -1,38 +1,55 @@
 const fs = require('fs');
 
-const arr = fs.readFileSync('/dev/stdin').toString().trim().split('\n').slice(1).map(v => v.split(' ').map(Number));
+const arr = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-console.log(
-  arr.map(([A, B]) => {
-    const queue = [[A, '']];
-    while (queue.length > 0) {
-      const [n, commands] = queue.shift();
+const logs = [];
 
-      if (n === B) {
-        return commands;
-      }
+for (let i = 1; i < arr.length; i += 1) {
+  const [A, B] = arr[i].split(' ').map(Number);
+  const queue = [[A, '']];
+  const isVisited = Array(10000).fill(false);
 
-      queue.push(
-        [D(n), commands + 'D'],
-        [S(n), commands + 'S'],
-        [L(n), commands + 'L'],
-        [R(n), commands + 'R'],
-      );
+  while (queue.length > 0) {
+    const [n, commands] = queue.shift();
+
+    if (n === B) {
+      logs.push(commands);
+      break;
     }
-  }).join('\n')
-)
 
+    const nextD = D(n)
+    if (isVisited[nextD] === false) {
+      isVisited[nextD] = true;
+      queue.push([nextD, commands + "D"]);
+    }
 
+    const nextS = S(n)
+    if (isVisited[nextS] === false) {
+      isVisited[nextS] = true;
+      queue.push([nextS, commands + "S"]);
+    }
+
+    const nextL = L(n)
+    if (isVisited[nextL] === false) {
+      isVisited[nextL] = true;
+      queue.push([nextL, commands + "L"]);
+    }
+
+    const nextR = R(n)
+    if (isVisited[nextR] === false) {
+      isVisited[nextR] = true;
+      queue.push([nextR, commands + "R"]);
+    }
+  }
+}
+
+console.log(logs.join('\n'));
 
 function D(n) {
-  n *= 2;
-  if (n >= 10000) n %= 10000;
-  return n;
+  return (n * 2) % 10000;
 }
 function S(n) {
-  n -= 1;
-  if (n === 0) n = 9999;
-  return n;
+  return (n === 0) ? 9999 : n - 1;
 }
 function L(n) {
   return (n % 1000) * 10 + Math.floor(n / 1000);
