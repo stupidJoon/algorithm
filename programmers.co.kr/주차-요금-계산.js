1187c90431423773ad6c40e0adb1cd5dc62d9d5f
JunSangYu <35032401+stupidJoon@users.noreply.github.com>
2022-12-31 14:48:39 +0000

    12.31

--- programmers.co.kr/주차-요금-계산.js
+++ programmers.co.kr/주차-요금-계산.js
@@ -21,7 +21,16 @@ function solution(fees, records) {
         const diff = (23 * 60 + 59) - (srcTime[0] * 60 + srcTime[1]);
         timeMap[number] += diff;
     });
-    console.log(timeMap, inMap);
+
+    const results = Object.entries(timeMap).map(([num, time]) => {
+        return [
+            num,
+            (time <= baseTime) ? baseFee : baseFee + Math.ceil((time - baseTime) / unitTime) * unitFee,
+        ];
+    }).sort((a, b) => Number(a[0]) - Number(b[0]));
+
+    return results.map(v => v[1]);
 }
+
 
 console.log(solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]))
