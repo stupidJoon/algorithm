89f18b34045cc9a80b7fab6af24ceb3d99b89ad3
JunSangYu <35032401+stupidJoon@users.noreply.github.com>
2023-01-10 14:45:16 +0000

    01.10

--- programmers.co.kr/순위-검색.js
+++ programmers.co.kr/순위-검색.js
@@ -1,32 +1,65 @@
 function solution(info, queries) {
-  const s = new Set();
-  const scoreMap = {};
+  const map = {};
 
-  for (const query of queries) {
+  function mapPush(k, v) {
+    if (map[k] === undefined) {
+      map[k] = [v];
+    } else {
+      map[k].push(v);
+    }
+  }
+
+  for (const i of info) {
+    let [language, position, career, food, score] = i.split(' ');
+    score = Number(score);
+
+    // java backend junior pizza
+    // - backend junior pizza
+    // - - junior pizza
+    // - - - pizza
+    // - - - -
+    // ...
+  }
+
+  return queries.map(query => {
     const [language, position, career, temp] = query.split('and').map(v => v.trim());
-    let [soulfood, score] = temp.split(' ');
+    let [food, score] = temp.split(' ');
+    score = Number(score);
 
+    const availables = map[`${language}${position}${career}${food}`];
 
-    const langArr = [];
-    for (const lang of (language === '-' ? ['cpp', 'java', 'python'] : [language])) {
-      langArr.push(lang);
-    }
-    const posArr = [];
-    for (const pos of (position === '-' ? ['backend', 'frontend'] : [position])) {
-      posArr.push(...langArr.map(v => v + pos));
-    }
-    const carArr = [];
-    for (const car of (career === '-' ? ['junior', 'senior'] : [career])) {
-      carArr.push(...posArr.map(v => v + car));
-    }
-    for (const soul of (soulfood === '-' ? ['chicken', 'pizza'] : [soulfood])) {
-      carArr.forEach(v => s.add(v + soul));
-    }
-    console.log(s)
+    console.log(query, availables)
+
+    if (availables === undefined) return 0;
+    availables.sort((a, b) => a - b);
+
+    return binarySearch(availables, score, 0, availables.length - 1);
+  });
+}
+
+function f(str, cnt, arr) {
+  if (cnt === 4) return arr;
+
+}
+
+function binarySearch(arr, threshold, left, right) {
+  if (left === arr.length) return 0;
+  if (left > right) return arr.length - left;
+
+  const mid = Math.floor((left + right) / 2);
+
+  if (arr[mid] >= threshold) {
+    return binarySearch(arr, threshold, left, mid - 1);
+  } else {
+    return binarySearch(arr, threshold, mid + 1, right);
   }
 }
 
+// 150 || 50 80 150 150 210 260
+
 solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"], ["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"])
+
+
 
 // function old_solution(info, queries) {
 //   const languageMap = { cpp: 0, java: 0, python: 0 };
