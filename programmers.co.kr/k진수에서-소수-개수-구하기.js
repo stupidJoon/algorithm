447cb5792d034e0abb1a5563622c7352395bb3e0
JunSangYu <35032401+stupidJoon@users.noreply.github.com>
2023-05-26 14:04:43 +0000

    0526

--- programmers.co.kr/k진수에서-소수-개수-구하기.js
+++ programmers.co.kr/k진수에서-소수-개수-구하기.js
@@ -1,14 +1,11 @@
+function solution(n, k) {
+    return n.toString(k).split('0').map(Number).filter(isPrime).length;
+}
+
 function isPrime(n) {
-    if (n === 1) return false;
+    if (n < 2) return false;
     for (let i = 2; i <= Math.sqrt(n); i += 1) {
         if (n % i === 0) return false;
     }
     return true;
 }
-
-function solution(n, k) {
-    const binary = n.toString(k);
-    return binary.split('0').filter((value) => value != 0).filter((value) => isPrime(parseInt(value))).length;
-}
-
-console.log(solution(437674, 3));
